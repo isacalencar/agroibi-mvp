efbd2f53ee3dff4cb73f8403f12e9be2e58fb70f
heitorthewizard <heitor.mb27@gmail.com>
2022-10-10 22:01:00 -0300

    added new logo and design

--- database.js
+++ database.js
@@ -6,7 +6,7 @@ const database = {
       image: "tomato.png",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem dolores quod itaque repellendus ex culpa quo, earum temporibus vero ea harum. Sequi fugiat corrupti incidunt perferendis vel voluptatibus non porro?",
-      price: 50,
+      price: '50/Caixa',
       seller: "Seu João",
       contact: "https://api.whatsapp.com/send?1=pt_BR&phone=558893205605",
       location: "Ubajara",
