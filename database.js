5d41d675379006d91f66a7f8334d58fe99d56eb5
heitorthewizard <heitor.mb27@gmail.com>
2022-10-10 22:52:03 -0300

    added some products as example

--- database.js
+++ database.js
@@ -3,24 +3,101 @@ const database = {
     // PRODUCT
     {
       name: "Tomate",
-      image: "tomato.png",
+      image: "tomate.jpg",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem dolores quod itaque repellendus ex culpa quo, earum temporibus vero ea harum. Sequi fugiat corrupti incidunt perferendis vel voluptatibus non porro?",
-      price: '50/Caixa',
+        "Exercitationem dolores quod itaque repellendus ex culpa quo, earum temporibus vero ea harum.",
+      price: "50/caixa",
       seller: "Seu João",
       contact: "https://api.whatsapp.com/send?1=pt_BR&phone=558893205605",
       location: "Ubajara",
     },
     // PRODUCT
     {
-      name: "Tomate Orgânico",
-      image: "tomato.png",
+      name: "Alface",
+      image: "alface.jpg",
       description:
-        "O mió tumati da regiaum",
-      price: 150,
+        "Exercitationem dolores quod itaque repellendus ex culpa quo, earum temporibus vero ea harum.",
+      price: "150/caixa",
       seller: "Isac Alencar",
       contact: 558893205605,
       location: "Tianguá",
-    }
+    },
+    // PRODUCT
+    {
+      name: "Banana",
+      image: "banana.jpg",
+      description:
+        "Exercitationem dolores quod itaque repellendus ex culpa quo, earum temporibus vero ea harum.",
+      price: "64.99/cacho",
+      seller: "Carlos Raimundão",
+      contact: 558893205605,
+      location: "Tianguá",
+    },
+    // PRODUCT
+    {
+      name: "Milho",
+      image: "milho.jpg",
+      description:
+        "Exercitationem dolores quod itaque repellendus ex culpa quo, earum temporibus vero ea harum.",
+      price: "62/caixa",
+      seller: "João Rafael",
+      contact: 558893205605,
+      location: "Tianguá",
+    },
+    // PRODUCT
+    {
+      name: "Pimentão",
+      image: "pimentao.jpg",
+      description:
+        "Exercitationem dolores quod itaque repellendus ex culpa quo, earum temporibus vero ea harum.",
+      price: "72.36/caixa",
+      seller: "Zé do Pimentão",
+      contact: 558893205605,
+      location: "Tianguá",
+    },
+    // PRODUCT
+    {
+      name: "Manga",
+      image: "mangas.jpg",
+      description:
+        "Exercitationem dolores quod itaque repellendus ex culpa quo, earum temporibus vero ea harum.",
+      price: "64.99/caixa",
+      seller: "Roberto Carlos",
+      contact: 558893205605,
+      location: "Tianguá",
+    },
+    // PRODUCT
+    {
+      name: "Batata",
+      image: "batatas.jpg",
+      description:
+        "Exercitationem dolores quod itaque repellendus ex culpa quo, earum temporibus vero ea harum.",
+      price: "28.50/caixa",
+      seller: "Calos Alencar",
+      contact: 558893205605,
+      location: "Ubajara",
+    },
+    // PRODUCT
+    {
+      name: "Melão",
+      image: "meloes.jpg",
+      description:
+        "Exercitationem dolores quod itaque repellendus ex culpa quo, earum temporibus vero ea harum.",
+      price: "38.50/caixa",
+      seller: "Margarida",
+      contact: 558893205605,
+      location: "Tianguá",
+    },
+    // PRODUCT
+    {
+      name: "Acerola",
+      image: "acerola.jpg",
+      description:
+        "Exercitationem dolores quod itaque repellendus ex culpa quo, earum temporibus vero ea harum.",
+      price: "99/caixa",
+      seller: "Rocine Fernandes",
+      contact: 558893205605,
+      location: "Ubajara",
+    },
   ],
 };
