b5518a35fcbc4e62e334a1d29d1eb9fa4313b485
heitorthewizard <heitor.mb27@gmail.com>
2022-11-12 14:58:47 -0300

    added more products

--- database.js
+++ database.js
@@ -12,5 +12,197 @@ const database = {
       location: "Tianguá",
       deliver: "Não",
     },
+    // PRODUCT
+    {
+      name: "Pimentão",
+      image: "pimentao-verde.jpg",
+      description:
+        "Para mais detalhes e negociação, entre em contato comigo clicando em comprar",
+      price: "30,00 / caixa",
+      seller: "Chiquinho Hortifrute",
+      contact: 558892167650,
+      location: "Tianguá",
+      deliver: "Sim",
+    },
+    // PRODUCT
+    {
+      name: "Batata Doce",
+      image: "batata-doce.jpg",
+      description:
+        "Para mais detalhes e negociação, entre em contato comigo clicando em comprar",
+      price: "60,00 / caixa",
+      seller: "Chiquinho Hortifruti",
+      contact: 558892167650,
+      location: "Tianguá",
+      deliver: "Sim",
+    },
+    // PRODUCT
+    {
+      name: "Pepino",
+      image: "pepino.jpg",
+      description:
+        "Para mais detalhes e negociação, entre em contato comigo clicando em comprar",
+      price: "45,00 / caixa",
+      seller: "Chiquinho Hortifruti",
+      contact: 558892167650,
+      location: "Tianguá",
+      deliver: "Sim",
+    },
+    // PRODUCT
+    {
+      name: "Maracujá",
+      image: "maracuja.jpg",
+      description:
+        "Para mais detalhes e negociação, entre em contato comigo clicando em comprar",
+      price: "65,00 / caixa",
+      seller: "Chiquinho Hortifruti",
+      contact: 558892167650,
+      location: "Tianguá",
+      deliver: "Sim",
+    },
+    // PRODUCT
+    {
+      name: "Chuchu",
+      image: "chuchu.jpg",
+      description:
+        "Para mais detalhes e negociação, entre em contato comigo clicando em comprar",
+      price: "20 / caixa",
+      seller: "Chiquinho Hortifruti",
+      contact: 558892167650,
+      location: "Tianguá",
+      deliver: "Sim",
+    },
+    // PRODUCT
+    {
+      name: "Feijão Verde",
+      image: "feijao-verde.jpg",
+      description:
+        "Para mais detalhes e negociação, entre em contato comigo clicando em comprar",
+      price: "35,00 / caixa",
+      seller: "Chiquinho Hortifruti",
+      contact: 558892167650,
+      location: "Tianguá",
+      deliver: "Sim",
+    },
+    // PRODUCT
+    {
+      name: "Abobrinha",
+      image: "abobrinha.jpg",
+      description:
+        "Para mais detalhes e negociação, entre em contato comigo clicando em comprar",
+      price: "50,00 / caixa",
+      seller: "Chiquinho Hortifruti",
+      contact: 558892167650,
+      location: "Tianguá",
+      deliver: "Sim",
+    },
+    // PRODUCT
+    {
+      name: "Berinjela",
+      image: "berinjela.jpg",
+      description:
+        "Para mais detalhes e negociação, entre em contato comigo clicando em comprar",
+      price: "25,00 / caixa",
+      seller: "Chiquinho Hortifruti",
+      contact: 558892167650,
+      location: "Tianguá",
+      deliver: "Sim",
+    },
+    // PRODUCT
+    {
+      name: "Vagem",
+      image: "vagem.jpg",
+      description:
+        "Para mais detalhes e negociação, entre em contato comigo clicando em comprar",
+      price: "40,00 / caixa",
+      seller: "Chiquinho Hortifruti",
+      contact: 558892167650,
+      location: "Tianguá",
+      deliver: "Sim",
+    },
+    // PRODUCT
+    {
+      name: "Pimentinha de cheiro",
+      image: "pimentinha-de-cheiro.jpg",
+      description:
+        "Para mais detalhes e negociação, entre em contato comigo clicando em comprar",
+      price: "20, 00 / caixa",
+      seller: "Chiquinho Hortifruti",
+      contact: 558892167650,
+      location: "Tianguá",
+      deliver: "Sim",
+    },
+    // PRODUCT
+    {
+      name: "Alface",
+      image: "alface.jpg",
+      description:
+        "Para mais detalhes e negociação, entre em contato comigo clicando em comprar",
+      price: "10,00 / caixa",
+      seller: "Chiquinho Hortifruti",
+      contact: 558892167650,
+      location: "Tianguá",
+      deliver: "Sim",
+    },
+    // PRODUCT
+    {
+      name: "Cheiro Verde",
+      image: "cheiro-verde.jpg",
+      description:
+        "Para mais detalhes e negociação, entre em contato comigo clicando em comprar",
+      price: "0,40 / molho",
+      seller: "Chiquinho Hortifruti",
+      contact: 558892167650,
+      location: "Tianguá",
+      deliver: "Sim",
+    },
+    // PRODUCT
+    {
+      name: "Macaxeira",
+      image: "macaxeira.jpg",
+      description:
+        "Para mais detalhes e negociação, entre em contato comigo clicando em comprar",
+      price: "35,00 / caixa",
+      seller: "Chiquinho Hortifruti",
+      contact: 558892167650,
+      location: "Tianguá",
+      deliver: "Sim",
+    },
+    // PRODUCT
+    {
+      name: "Alcega",
+      image: "alcega.jpg",
+      description:
+        "Para mais detalhes e negociação, entre em contato comigo clicando em comprar",
+      price: "20,00 / caixa",
+      seller: "Chiquinho Hortifruti",
+      contact: 558892167650,
+      location: "Tianguá",
+      deliver: "Sim",
+    },
+    // PRODUCT
+    {
+      name: "Maxixe",
+      image: "maxixe.jpg",
+      description:
+        "Para mais detalhes e negociação, entre em contato comigo clicando em comprar",
+      price: "45,00 / caixa",
+      seller: "Chiquinho Hortifruti",
+      contact: 558892167650,
+      location: "Tianguá",
+      deliver: "Sim",
+    },
+    // PRODUCT
+    {
+      name: "Quiabo",
+      image: "quiabo.jpg",
+      description:
+        "Para mais detalhes e negociação, entre em contato comigo clicando em comprar",
+      price: "30,00 / caixa",
+      seller: "Chiquinho Hortifruti",
+      contact: 558892167650,
+      location: "Tianguá",
+      deliver: "Sim",
+    },
   ],
 };
